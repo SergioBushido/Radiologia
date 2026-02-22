81c42590eb14c055201ec99d441ca8ede045c809
Sergio Fernández Jiménez <84178657+SergioBushido@users.noreply.github.com>
2026-02-22 11:33:30 +0000

    fix: only count unread messages where user is the receiver

--- pages/api/messages/unread.ts
+++ pages/api/messages/unread.ts
@@ -9,6 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const count = await prisma.message.count({
         where: {
+            receiverId: user.id,
             isRead: false
         }
     })
